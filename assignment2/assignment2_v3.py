0314509892dd661ac30910de85688fc02e63be24
Kern <kerntallett@gmail.com>
2024-03-11 13:15:27 +0000

    Pylint fixes
    Fixes on whitespace, docstrings and line lengths to improve pylint score.

--- assignment2/assignment2_v3.py
+++ assignment2/assignment2_v3.py
@@ -2,8 +2,8 @@
 
 # MIT Licensed
 
-# Python to calculate the multidimensional normal using the monte carlo using MPI reduce and SUM to parallelize the process.
-
+# Python to calculate the multidimensional normal using the monte carlo
+# Using MPI reduce and SUM to parallelize the process.
 # Run with 'mpirun -n 4 python assignment2_v3.py'
 
 # Import statements
@@ -15,26 +15,22 @@ import numpy as np
 
 
 # Start of class
-
 class MonteCarloSimulation:
     def __init__(self, num_simulations, num_variables, global_seed=None):
-        
         # Set class properties
-        
-        
         self.num_simulations = num_simulations
         self.num_variables = num_variables
         self.global_seed = global_seed
-        
         # Set properties based on no of cores being used
-        
         self.comm = MPI.COMM_WORLD
         self.rank = self.comm.Get_rank()
         self.size = self.comm.Get_size()
 
     # Generates samples from seed, dimension and function
-    
     def _worker(self, seed, dimension, function, *args):
+        """
+        This function creates the samples for the simulation
+        """
         np.random.seed(seed)
         if dimension == 1:
             samples = np.random.normal(loc=0, scale=1.0, size=self.num_simulations // self.size)
@@ -42,98 +38,89 @@ class MonteCarloSimulation:
             samples = np.random.multivariate_normal(mean=np.zeros(self.num_variables), cov=np.eye(self.num_variables), size=self.num_simulations // self.size)
         else:
             raise ValueError("Unsupported dimension")
-    
         evaluations = function(samples, *args)
         local_mean = np.mean(evaluations)
         local_var = np.var(evaluations)
         return samples, local_mean, local_var
-    
     # Calculates errors of the integral
-    
     def _compute_error(self, integral_values):
+        """
+        This function calculates the errors of the integral
+        """
         global_integral = np.zeros_like(integral_values)
         self.comm.Allreduce(integral_values, global_integral, op=MPI.SUM)
         global_integral /= self.size
-
         # Compute error
         squared_diff = (integral_values - global_integral) ** 2
         mean_squared_diff = np.mean(squared_diff)
         error = np.sqrt(mean_squared_diff / (self.size - 1))
         return error
-
-    # Integrate function 
-    
+    # Integrate function
     def run_integration(self, dimension, function, *args):
+        """"
+        
+        This function calculates the integral using MPI reduce to parallize the integration
+        """
         local_seed = np.random.randint(0, 10000) if self.global_seed is None else self.global_seed + self.rank
         samples, local_mean, local_var = self._worker(local_seed, dimension, function, *args)
-        
         # Set global variable matrices
         global_samples = np.zeros_like(samples)
         global_mean = np.zeros_like(local_mean)
         global_var = np.zeros_like(local_var)
-    
         self.comm.Allreduce(samples, global_samples, op=MPI.SUM)
         self.comm.Allreduce(local_mean, global_mean, op=MPI.SUM)
         self.comm.Allreduce(local_var, global_var, op=MPI.SUM)
-    
         global_samples /= self.size
         global_mean /= self.size
         global_var /= self.size
-    
         error = self._compute_error(local_mean)
-    
         return global_samples, global_mean, global_var, error
 
 # Gaussian function to compute
-def normal_pdf(x_vals, mu, sigma):
-
+def normal_pdf(x_vals, mu_mv, sigma):
+    """
+    
+    Gaussian function
+    
+    """
     # Calculating the coefficient of the integral
-    coefficient = 1 / (math.sqrt((2 * math.pi) ** len(mu) * np.linalg.det(sigma)))
-    
+    coefficient = 1 / (math.sqrt((2 * math.pi) ** len(mu_mv) * np.linalg.det(sigma)))
     # Making sure the dimensions are the same size
-    centered_samples = x_vals - mu.reshape(1, -1)
-    
+    centered_samples = x_vals - mu_mv.reshape(1, -1)
     # Calculating the exponent of the integral
     exponent = -0.5 * np.sum(np.dot(centered_samples, np.linalg.inv(sigma)) * centered_samples, axis=1)
-    
     # Combining the two to make the complete equation
     pdf_val = coefficient * np.exp(exponent)
-    
     return pdf_val
 
 # Using the class and functions in main function
 def main():
 
+    """
+    Main function to utilise the class features 
+    to calculate the integral of the gaussian distribution function
+    """
     start_time = time.time()
-
     # Number of samples
     num_simulations = 1000000
-    
     # EDIT NUMBER OF DIMENSIONS
     num_variables = 6
     global_seed = 12
-    
-    
     mc_sim = MonteCarloSimulation(num_simulations, num_variables, global_seed)
-    
     # EDIT: MEAN VECTOR SHIFT DISTRIBUTION
-    #mu = np.zeros(num_variables)
-    mu = np.array([-3, -2, -1, 0, 1, 2, 3])
-    
-    # EDIT: SPREAD 
+    mu_mv = np.zeros(num_variables)
+    #mu_mv = np.array([-3, -2, -1, 0, 1, 2, 3])
+    # EDIT: SPREAD
     #sigma = np.eye(num_variables)
     sigma = np.diag([1, 2, 3, 4, 5, 6])
-
     # Evaluate integral, mean, variance, and error
-    samples, mean, variance, error = mc_sim.run_integration(num_variables, normal_pdf, mu, sigma)
-    
+    samples, mean, variance, error = mc_sim.run_integration(num_variables, normal_pdf, mu_mv, sigma)
     # Prints results if back at global
     if mc_sim.rank == 0:
         print("Worker Mean:", mean)
         print("Worker Variance:", variance)
         print("Integral of Normal Distribution:", np.mean(samples))
         print("Error in Integral:", error)
-    
     end_time = time.time()
     time_taken = end_time - start_time
     print("Time Taken:",time_taken)
